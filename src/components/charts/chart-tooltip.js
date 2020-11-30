5e7f1d4e9c2aef93f5cf8e24b7e5836b99242c90
Matt Watson <mattwat@gmail.com>
2020-11-30 16:19:31 -0500

    style tooltip

--- src/components/charts/chart-tooltip.js
+++ src/components/charts/chart-tooltip.js
@@ -3,18 +3,16 @@ import styled from 'styled-components'
 
 //
 
-const Wrapper = styled.div`
+const Wrapper = styled.div(({ color }) => `
+  font-size: 90%;
   background-color: #fff;
   display: flex;
   border-radius: 3px;
   filter: drop-shadow(0 0 4px rgba(0, 0, 0, 0.2));
+  border-width: 0 0 0 1rem;
+  border-style: solid;
+  border-color: ${ color };
   overflow: hidden;
-`
-
-const ColorBlock = styled.div(({ color }) => `
-  width: 1rem;
-  min-height: 100%;
-  background-color: ${ color };
 `)
 
 const Details = styled.div`
@@ -27,19 +25,18 @@ const Details = styled.div`
     color: #666;
   }
   & .label {
-    padding: 0.25rem 0.5rem;
+    padding: 0.5rem 0.5rem 0.1rem 0.5rem;
     font-weight: bold;
   }
   & .value {
-    padding: 0.25rem 0.5rem;
+    padding: 0.1rem 0.5rem 0.5rem 0.5rem;
   }
 `
 
 export const ChartTooltip = ({ datum, grouping }) => {
   const { label, value, color } = datum
   return (
-    <Wrapper>
-      <ColorBlock color={ color } />
+    <Wrapper color={ color }>
       <Details>
         <div className="grouping">{ grouping }</div>
         <div className="label">{ label }</div>
