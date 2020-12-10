323f4e2611ba92d25f5530249da6ea87c366a4d7
Matt Watson <mattwat@gmail.com>
2020-12-10 00:21:04 -0500

    JSON.stringify fields in csv download

--- src/utils/download-csv.js
+++ src/utils/download-csv.js
@@ -1,15 +1,22 @@
-function downloadCSV(data, filename = 'studies-export.csv') {
+function jsonToCsv(objArray) {
   let csv = 'data:text/csv;charset=utf-8,'
   const columnDelimiter = ','
-  const lineDelimiter = '\n'
-  const columns = Object.keys(data[0])
+  const lineDelimiter = '\r\n'
+  const columns = Object.keys(objArray[0])
   // add column headers
   csv += columns.join(columnDelimiter) + lineDelimiter
-  // add rows `data,data,data,\n`
-  data.forEach(rowObject => {
-    const nextLine = columns.map(column => rowObject[column]).join(columnDelimiter)
+  // add rows `value1,value2,value3\n`
+  objArray.forEach(rowObject => {
+    const nextLine = columns
+      .map(column => JSON.stringify(rowObject[column]).replace(/,/g, '; '))
+      .join(columnDelimiter)
     csv += nextLine + lineDelimiter
   })
+  return csv  
+}
+
+function downloadCSV(data, filename = 'studies-export.csv') {
+  const csv = jsonToCsv(data)
   const link = document.createElement('a')
   link.setAttribute('href', encodeURI(csv))
   link.setAttribute('download', filename)
