443673655fa2ea7e78dd63a0586512a63f3d5068
Matt Watson <mattwat@gmail.com>
2020-12-16 15:49:00 -0500

    style table expansion panels

--- src/components/data-table/expansion-panel.js
+++ src/components/data-table/expansion-panel.js
@@ -4,10 +4,14 @@ import { Subheading } from '../typography'
 import { ExternalLink } from '../link'
 
 const Wrapper = styled.div`
-  padding: 1rem;
-  background-color: #eee;
+  background-color: #cccccc55;
   border-left: 0.5rem solid var(--color-lightgrey);
   font-size: 80%;
+  transition: border-color 250ms, background-color 250ms;
+  &:hover {
+    border-color: var(--color-blueberry);
+    background-color: #f0eeee66;
+  }
 `
 
 const LIST_DELIMITER = ' ; '
@@ -18,20 +22,54 @@ const DbGapLink = ({ pht }) => {
   return <ExternalLink to={ `https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/dataset.cgi?study_id=phs000956.v4.p1&pht=${ digits }` }>{ pht }</ExternalLink>
 }
 
+const Header = styled.div`
+  display: flex;
+  gap: 2rem;
+  justify-content: space-between;
+  align-items: flex-end;
+  padding: 1rem;
+  margin: 0;
+  border-bottom: 1px solid #ccc;
+  line-height: 1.4;
+  background-color: #d1d1d1;
+  & ${ Subheading } {
+    margin: 0;
+  }
+  & span { }
+`
+
+const Body = styled.div`
+  padding: 1rem 0;
+  margin: 0 1rem;
+  line-height: 1.4;
+`
+
+const Footer = styled.div`
+  background-color: #d1d1d1;
+  height: 2px;
+`
+
 export const ExpansionPanel = ({ data }) => {
   return (
     <Wrapper>
-      <Subheading>
-        <ExternalLink to={ `https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/study.cgi?study_id=${ data.Accession }` }>{ data.Name } ({ data.Accession })</ExternalLink>
-      </Subheading>
-      <hr />
-      <strong>Study Description:</strong> { data.Description }<br /><br />
-      <strong>Primary Research Focus:</strong> { data.Primary_Research_Focus }<br /><br />
-      <strong>dbGaP Listed Variable:</strong> { data.dbGaP_Listed_Variable.join(LIST_DELIMITER) }<br /><br />
-      <strong>Study Type:</strong> { data.Type.join(LIST_DELIMITER) }<br /><br />
-      <strong>Study-Reported Population(s):</strong> { data.Populations.join(LIST_DELIMITER) }<br /><br />
-      <strong>Study Consent:</strong> { data.Consent_Short.join(LIST_DELIMITER) }<br /><br />
-      <strong>Primary Data Dictionary Link:</strong> <DbGapLink pht={ data.Data_Dictionary_Link }/><br />
+      <Header>
+        <Subheading>
+          <ExternalLink to={ `https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/study.cgi?study_id=${ data.Accession }` }>{ data.Name }</ExternalLink>
+        </Subheading>
+        <span>{ data.Accession }</span>
+      </Header>
+
+      <Body>
+        <strong>Study Description:</strong> { data.Description }<br /><br />
+        <strong>Primary Research Focus:</strong> { data.Primary_Research_Focus }<br /><br />
+        <strong>dbGaP Listed Variable:</strong> { data.dbGaP_Listed_Variable.join(LIST_DELIMITER) }<br /><br />
+        <strong>Study Type:</strong> { data.Type.join(LIST_DELIMITER) }<br /><br />
+        <strong>Study-Reported Population(s):</strong> { data.Populations.join(LIST_DELIMITER) }<br /><br />
+        <strong>Study Consent:</strong> { data.Consent_Short.join(LIST_DELIMITER) }<br /><br />
+        <strong>Primary Data Dictionary Link:</strong> <DbGapLink pht={ data.Data_Dictionary_Link }/><br />
+      </Body>
+
+      <Footer />
     </Wrapper>
   )
 }
